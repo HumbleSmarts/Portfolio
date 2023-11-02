7e0081a48a278cca5dff693064e2a7b214e14d6f
humblesmarts <humblesmarts11@gmail.com>
2023-11-02 17:59:57 +0100

    updated about page

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -6,29 +6,28 @@ import {
   SiSlack,
   SiVercel,
   SiGithub,
-
   SiMacos,
 } from "react-icons/si";
 
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiMacos /> Macos
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiVisualstudiocode /> VsCode
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiPostman /> Postman
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiSlack /> Slack
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiGithub /> Github
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiVercel /> Vercel
       </Col>
     </Row>
