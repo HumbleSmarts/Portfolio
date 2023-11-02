5091b49e933ebf90bc8396cd1f9e7ae5942a0217
humblesmarts <humblesmarts11@gmail.com>
2023-11-02 17:56:58 +0100

    updated about page

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -19,38 +19,38 @@ import {
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <DiPython /> Python
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <DiReact /> Reactjs
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiMysql /> Mysql
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <DiDjango /> Django
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <DiJavascript1 /> Javascript
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <DiGit /> Git
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <DiNodejs /> NodeJs
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiFirebase /> Firebase
       </Col>
 
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiPostgresql /> Postgresql
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiPytorch /> Pytorch
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      <Col xs={4} md={3} className="tech-icons">
         <SiNumpy /> Numpy
       </Col>
     </Row>
